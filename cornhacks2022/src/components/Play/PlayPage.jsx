e5c49257d995d977e7a182890fef58c82c5155d6
EllissaPeterson <ellissa@iastate.edu>
2022-01-23 10:36:54 -0600

    updates

--- cornhacks2022/src/components/Play/PlayPage.jsx
+++ cornhacks2022/src/components/Play/PlayPage.jsx
@@ -4,14 +4,15 @@ import '../../App.css';
 import Header from "../Header/Header";
 import catTama from "../../art/cat gotchi.png";
 import catNeutral from "../../art/catneutral.gif";
-import Code from "./PetWindow/Code/Code";
 
 function PlayPage() {
-    const [num, setNum] = useState('0');
-    const [level, setLevel] = useState('1');
-    const [hunger, setHunger] = useState('0');
-    const [happy, setHappy] = useState('0');
-    const [know, setKnow] = useState('0');
+    const [num, setNum] = useState("msk9HaS6Ic");
+    const [level, setLevel] = useState(1);
+    const [hunger, setHunger] = useState(0);
+    const [happy, setHappy] = useState(0);
+    const [know, setKnow] = useState(0);
+
+    const problem = <div className="se-widget" data-widget={num}/>;
 
     return (
         <div className="play">
@@ -41,21 +42,20 @@ function PlayPage() {
                 <div className="currKnowledge">
                     <h6>{know}</h6>
                 </div>
-                <Code />
+                <div className="code">
+                    {num}
+                    <div className="se-widget" data-widget={num}/>
+                </div>
             </div>
             <div className="completedChallenges">
-                <Button className="button" id="1" block color="primary" onClick={() => setNum('1')}>Objective 1</Button>{' '}
-                <Button className="button" id="2" block color="primary" onClick={() => setNum('2')}>Objective 2</Button>{' '}
-                <Button className="button" id="3" block color="primary" onClick={() => setNum('3')}>Objective 3</Button>{' '}
-                <Button className="button" id="4" block color="primary" onClick={() => setNum('4')}>Objective 4</Button>{' '}
-                <Button className="button" id="5" block color="primary" onClick={() => setNum('5')}>Objective 5</Button>{' '}
-                <Button className="button" id="6" block color="primary" onClick={() => setNum('6')}>Objective 6</Button>{' '}
-                <Button className="button" id="7" block color="primary" onClick={() => setNum('7')}>Objective 7</Button>{' '}
-                <Button className="button" id="8" block color="primary" onClick={() => setNum('8')}>Objective 8</Button>{' '}
-                <Button className="button" id="9" block color="primary" onClick={() => setNum('9')}>Objective 9</Button>{' '}
-                <Button className="button" id="10" block color="primary" onClick={() => setNum('10')}>Objective 10</Button>{' '}
-                <Button className="button" id="11" block color="primary" onClick={() => setNum('11')}>Objective 11</Button>{' '}
-                <Button className="button" id="12" block color="primary" onClick={() => setNum('12')}>Objective 12</Button>{' '}
+                <Button className="button" id="1" block color="primary" onClick={() => setNum("msk9HaS6Ic")}>Objective 1</Button>{' '}
+                <Button className="button" id="2" block color="primary" onClick={() => setNum("3TCnmBtjk6")}>Objective 2</Button>{' '}
+                <Button className="button" id="3" block color="primary" onClick={() => setNum("fksvScNZpf")}>Objective 3</Button>{' '}
+                <Button className="button" id="4" block color="primary" onClick={() => setNum("IvAExOrIfy")}>Objective 4</Button>{' '}
+                <Button className="button" id="5" block color="primary" onClick={() => setNum("flDt28zl4C")}>Objective 5</Button>{' '}
+            </div>
+            <div className="manual">
+                <Button className="upgrade" id="5" block color="primary" onClick={() => {setLevel(level + 1); setHappy(happy + 3); setKnow(know + 2); setHunger(hunger + 1)}}>Level Up</Button>{' '}
             </div>
         </div>
     );
